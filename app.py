30c4e6d7462fc557d84c981bf895c42f5e1566dd
Gabriel Schneider <gabriel.schneider.almeida@gmail.com>
2018-04-12 19:36:09 -0300

    Refactor main app code

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as pyplot
 import random
 import time
-
+from datetime import datetime
+import csv
+import os
 
 from lotkavolterra.core import Simulation, Population, Solution
 from genetic.selection import TournamentSelection
@@ -15,10 +17,9 @@ def main():
     random.seed()
 
     population = Population(1000, SmallerIsBetterRule())
+    population.populate()
 
     environment = Simulation(prey=80, predator=30)
-
-    population.populate()
     population.evaluate(environment)
 
     crossover = TwoPointCrossover()
@@ -27,12 +28,37 @@ def main():
 
     stats = []
 
-    generations = 1000
-    step = 100
+    def export(id):
 
-    start = time.time()
+        # CSV export
+        with open(f'data/{id}/generation_{generation}.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Generation', 'Prey', 'Predator'])
+            for i, stat in enumerate(environment.stats):
+                writer.writerow([i, stat[0], stat[1]])
+
+        # Plot
+        x, y = zip(*environment.stats)
+        pyplot.clf()
+        pyplot.plot(x, 'r-', label='Prey')
+        pyplot.plot(y, 'b-', label='Predator')
+        pyplot.savefig(f'data/{id}/generation_{generation}.png')
+
+    started_at = time.time()
+    run_id = datetime.now().strftime('%Y%m%d_%H%M%S')
+    os.makedirs(f'data/{run_id}')
+
     try:
+        generations = 10000
+        export_interval = 100
         for generation in range(generations):
+
+            # Check equilibrium
+            if population.best.fitness == 0:
+                print('[!] Equilibrium found!')
+                raise KeyboardInterrupt
+
+            # Select, crossover and mutate!
             solutions_to_crossover = population.select(
                 4, selector)
             for solution_set in solutions_to_crossover:
@@ -43,21 +69,25 @@ def main():
                     offspring.fitness = environment.evaluate(offspring, 1000)
                     population.insert(offspring)
             population.cap()
-            if generation % step == 0:
+
+            if generation % export_interval == 0:
+                environment.evaluate(population.best, 64, True)
+                export(run_id)
+
+                # Algorithm statistics
                 sum_of_fitness = sum(
                     x.fitness for x in population.solutions.values())
                 stats.append(sum_of_fitness)
                 print(
                     f'Generation {generation}... ({population.best.fitness}, {sum_of_fitness})')
+
     except KeyboardInterrupt:
+        export(run_id)
         print(population.best, sum(
             x.fitness for x in population.solutions.values()))
 
-    print(f'Duration: {time.time() - start}')
-    pyplot.plot(stats)
-    pyplot.xlabel('Generations')
-    pyplot.ylabel('Population Fitness')
-    pyplot.show()
+    finished_at = time.time()
+    print(f'Run duration: {math.floor(finished_at - started_at)} seconds')
 
 
 if __name__ == '__main__':
